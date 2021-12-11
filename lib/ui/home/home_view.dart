bc8cd53aa583fca3f8634fa2662061d72b7bf372
armatura24 <armaturapengva2222@gmail.com>
2021-12-11 19:58:33 +0200

    intl init

--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:h_reader/generated/l10n.dart';
 import 'package:h_reader/utils/app_colors.dart';
 
 class HomeView extends StatelessWidget {
@@ -8,15 +9,15 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: MediaQuery.of(context).padding.top * 2,
+        toolbarHeight: 50,
         shape: const RoundedRectangleBorder(
             borderRadius: BorderRadius.only(
                 bottomLeft: Radius.circular(8.0), bottomRight: Radius.circular(8.0))),
         backgroundColor: AppColors.accentGreen,
         centerTitle: true,
-        title: Text('HReader'),
+        title: Text(S.current.app_name),
       ),
-      body: Text('home'),
+      body: const SizedBox(),
     );
   }
 }
