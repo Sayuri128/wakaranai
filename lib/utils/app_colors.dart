a47f614ade768c591611e5106b02bda7047a7e14
armatura24 <armaturapengva2222@gmail.com>
2021-12-11 23:14:35 +0200

    feature: gallery_view -> render doujinshi card. implement gallery pagination

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static final Color mainBlack = Colors.black.withOpacity(0.8);
   static final Color green = Colors.green;
   static final Color accentGreen = Colors.greenAccent;
+  static final Color mainGrey = Colors.grey;
 }
