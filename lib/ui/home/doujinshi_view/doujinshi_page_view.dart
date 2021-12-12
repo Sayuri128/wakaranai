48a4c8c4a08a7fbbb867c96470b61e98b7e52fdb
armatura24 <vivcharenko10@gmail.com>
2021-12-12 13:42:49 +0200

    feature: gallery_view -> render doujinshi page previews

---
+++ lib/ui/home/doujinshi_view/doujinshi_page_view.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:h_reader/models/nhentai/doujinshi/pages_item/pages_item.dart';
+import 'package:h_reader/utils/nhentai_urls.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+class DoujinshiPageView extends StatefulWidget {
+  const DoujinshiPageView({Key? key, required this.mediaId, required this.pages}) : super(key: key);
+
+  final String mediaId;
+  final List<PagesItem> pages;
+
+  @override
+  State<DoujinshiPageView> createState() => _DoujinshiPageViewState();
+}
+
+class _DoujinshiPageViewState extends State<DoujinshiPageView> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Wrap(
+        spacing: 8,
+        runSpacing: 8,
+        children: List.generate(widget.pages.length, (index) => _buildPageItem(widget.pages[index])),
+      ),
+    );
+  }
+
+  Widget _buildPageItem(PagesItem item) {
+    return FractionallySizedBox(
+      widthFactor: 0.45,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8.0),
+        child: FadeInImage(
+            placeholder: Image.memory(kTransparentImage).image,
+            fit: BoxFit.fitHeight,
+            image: Image.network(
+                    NHentaiUrls.pageItem(widget.mediaId, item.t, widget.pages.indexOf(item) + 1))
+                .image),
+      ),
+    );
+  }
+}
