b941f1153a1388225430d70efc4e8da29ce1127b
armatura24 <vivcharenko10@gmail.com>
2021-12-17 21:59:56 +0200

    feature: doujinshi_viewer -> use PhotoViewGallery instead page view. also it preloads pages.

--- lib/ui/home/doujinshi_view/doujinshi_source_view.dart
+++ lib/ui/home/doujinshi_view/doujinshi_source_view.dart
@@ -2,7 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:h_reader/models/nhentai/doujinshi/pages_item/pages_item.dart';
 import 'package:h_reader/utils/nhentai_urls.dart';
-import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
 
 class DoujinshiSourceViewData {
   final int initialPage;
@@ -36,23 +36,16 @@ class _DoujinshiSourceViewState extends State<DoujinshiSourceView> {
 
   @override
   Widget build(BuildContext context) {
-    return PageView.builder(
-        controller: pageController,
+    return PhotoViewGallery.builder(
+        allowImplicitScrolling: true,
+        pageController: pageController,
+        scrollPhysics: const BouncingScrollPhysics(),
         itemCount: widget.data.pages.length,
-        itemBuilder: (_, index) => _buildPageItem(widget.data.pages[index]));
-  }
-
-  Widget _buildPageItem(PagesItem item) {
-    return PhotoView(
-        minScale: 0.3,
-        loadingBuilder: (context, event) => Center(
-              child: CircularProgressIndicator(
-                value: event == null
-                    ? 0
-                    : event.cumulativeBytesLoaded / (event.expectedTotalBytes ?? 0),
-              ),
-            ),
-        imageProvider: CachedNetworkImageProvider(NHentaiUrls.pageItemSource(
-            widget.data.mediaId, item.t, widget.data.pages.indexOf(item) + 1)));
+        builder: (context, index) {
+          return PhotoViewGalleryPageOptions(
+              minScale: 0.1,
+              imageProvider: CachedNetworkImageProvider(NHentaiUrls.pageItemSource(
+                  widget.data.mediaId, widget.data.pages[index].t, index + 1)));
+        });
   }
 }
