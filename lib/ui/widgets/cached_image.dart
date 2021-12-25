6de26e6468121b4e3145654ebf0aa136c21632e6
armatura24 <armaturapengva2222@gmail.com>
2021-12-26 00:46:18 +0200

    feature: custom_cache -> custom cache image provider

--- lib/ui/widgets/cached_image.dart
+++ lib/ui/widgets/cached_image.dart
@@ -1,3 +1,8 @@
+import 'dart:typed_data';
+import 'dart:ui';
+
+import 'package:http/http.dart' as http;
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:h_reader/blocs/image_cache/image_cache_cubit.dart';
@@ -24,11 +29,55 @@ class CachedImage extends StatelessWidget {
         child: BlocBuilder<ImageCacheCubit, ImageCacheState>(
           builder: (context, state) {
             if (state is ImageCacheSaved) {
-              return Image.memory(state.data);
+              return Image.memory(state.data, width: width, height: height, fit: fit);
             } else {
               return buildImageSkeletonLoader(height: height, width: width);
             }
           },
         ));
+  }
+}
+
+class CachedImageProvider extends ImageProvider<CachedImageProvider> {
+  final String url;
+
+  const CachedImageProvider({
+    required this.url,
+  });
+
+  @override
+  ImageStreamCompleter load(CachedImageProvider key, DecoderCallback decode) {
+    return MultiFrameImageStreamCompleter(
+      scale: 1.0,
+      codec: _loadAsync(decode),
+      informationCollector: () sync* {
+        yield ErrorDescription('Path: $url');
+      },
+    );
+  }
+
+  Future<Codec> _loadAsync(DecoderCallback decode) async {
+    final file = await ImageCacheCubit.instance.getFileFromCache(url);
+
+    Uint8List? bytes;
+
+    if (file == null) {
+      bytes = (await http.get(Uri.parse(url))).bodyBytes;
+      ImageCacheCubit.instance.putFile(url, bytes);
+    } else {
+      bytes = file.file.readAsBytesSync();
+    }
+
+    if (bytes.lengthInBytes == 0) {
+      PaintingBinding.instance?.imageCache?.evict(this);
+      throw StateError('$url is empty and cannot be loaded as an image.');
+    }
+
+    return await decode(bytes);
+  }
+
+  @override
+  Future<CachedImageProvider> obtainKey(ImageConfiguration configuration) {
+    return SynchronousFuture<CachedImageProvider>(this);
   }
 }
