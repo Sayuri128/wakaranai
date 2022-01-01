cddf42cce433cc84fff591de66710a59f5ea68da
armatura24 <vivcharenko10@gmail.com>
2022-01-01 22:38:41 +0200

    feature: service_viewer -> render gallery view

--- lib/blocs/api_client_controller/api_client_controller_state.dart
+++ lib/blocs/api_client_controller/api_client_controller_state.dart
@@ -4,19 +4,23 @@ class ApiClientControllerState {
 
   final ApiClient client;
   final ConfigInfo? configInfo;
+  final List<GalleryView>? galleryViews;
 
   const ApiClientControllerState({
     required this.client,
-    this.configInfo
+    this.configInfo,
+    this.galleryViews
   });
 
   ApiClientControllerState copyWith({
     ApiClient? client,
     ConfigInfo? configInfo,
+    List<GalleryView>? galleryViews,
   }) {
     return ApiClientControllerState(
       client: client ?? this.client,
       configInfo: configInfo ?? this.configInfo,
+      galleryViews: galleryViews ?? this.galleryViews,
     );
   }
 }
