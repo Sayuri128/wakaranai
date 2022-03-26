cb10fb6b0c64a45a7bf4124149716e734a29abc4
Armatura24 <vivcharenko2222@icloud.com>
2022-03-26 02:13:17 +0200

    feature: gallery_view -> on scroll load next page

--- lib/blocs/api_client_controller/api_client_controller_state.dart
+++ lib/blocs/api_client_controller/api_client_controller_state.dart
@@ -11,20 +11,26 @@ class ApiClientControllerState {
 class ApiClientControllerConfigInfo extends ApiClientControllerState {
   final ConfigInfo configInfo;
 
-  ApiClientControllerConfigInfo({required ApiClient client, required this.configInfo})
+  ApiClientControllerConfigInfo(
+      {required ApiClient client, required this.configInfo})
       : super(client: client);
 }
 
 class ApiClientControllerGalleryView extends ApiClientControllerState {
   final List<GalleryView> galleryViews;
+  final int currentPage;
 
-  ApiClientControllerGalleryView({required ApiClient client, required this.galleryViews})
+  ApiClientControllerGalleryView(
+      {required ApiClient client,
+      required this.galleryViews,
+      required this.currentPage})
       : super(client: client);
 }
 
 class ApiClientControllerConcreteView extends ApiClientControllerState {
   final ConcreteView concreteView;
 
-  ApiClientControllerConcreteView({required ApiClient client, required this.concreteView})
+  ApiClientControllerConcreteView(
+      {required ApiClient client, required this.concreteView})
       : super(client: client);
 }
--- lib/ui/service_viewer/service_viewer.dart
+++ lib/ui/service_viewer/service_viewer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:wakaranai/blocs/api_client_controller/api_client_controller_cubit.dart';
 import 'package:wakaranai/ui/service_viewer/gallery_view_card.dart';
 import 'package:wakaranai/utils/app_colors.dart';
@@ -9,49 +10,83 @@ import '../routes.dart';
 import 'concrete_viewer/concrete_viewer.dart';
 import 'package:wakaranai_json_runtime/models/gallery_view/gallery_view.dart';
 
-class ServiceView extends StatelessWidget {
+class ServiceView extends StatefulWidget {
   const ServiceView({Key? key, required this.apiClient}) : super(key: key);
 
   final ApiClient apiClient;
 
   @override
+  State<ServiceView> createState() => _ServiceViewState();
+}
+
+class _ServiceViewState extends State<ServiceView> {
+  final RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+
+  @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () {
-        Navigator.of(context).pushNamedAndRemoveUntil(Routes.home, (route) => false);
+        Navigator.of(context)
+            .pushNamedAndRemoveUntil(Routes.home, (route) => false);
         return Future.value(false);
       },
       child: MultiBlocProvider(
         providers: [
           BlocProvider<ApiClientControllerCubit>(
-              create: (context) => ApiClientControllerCubit(apiClient: apiClient)
-                ..getConfigInfo()
-                ..getGallery(1))
+              create: (context) =>
+                  ApiClientControllerCubit(apiClient: widget.apiClient)
+                    ..getConfigInfo()
+                    ..getGallery())
         ],
-        child: Scaffold(
-          backgroundColor: AppColors.backgroundColor,
-          extendBodyBehindAppBar: true,
-          body: ListView(
-            physics: const BouncingScrollPhysics(),
-            children: [
-              BlocBuilder<ApiClientControllerCubit, ApiClientControllerState>(
-                builder: (context, state) {
-                  if (state is ApiClientControllerGalleryView) {
-                    return Wrap(
-                      children: state.galleryViews
-                          .map((e) => GalleryViewCard(
-                              data: e,
-                              onTap: () {
-                                _onGalleryViewClick(context, e);
-                              }))
-                          .toList(),
-                    );
-                  } else {
-                    return const SizedBox();
-                  }
-                },
-              )
-            ],
+        child: BlocListener<ApiClientControllerCubit, ApiClientControllerState>(
+          listener: (context, state) {
+            if (state is ApiClientControllerGalleryView) {
+              _refreshController.loadComplete();
+            }
+          },
+          child: Scaffold(
+            backgroundColor: AppColors.backgroundColor,
+            extendBodyBehindAppBar: true,
+            body:
+                BlocBuilder<ApiClientControllerCubit, ApiClientControllerState>(
+              builder: (context, state) {
+                return SmartRefresher(
+                    enablePullUp: true,
+                    enablePullDown: false,
+                    footer: CustomFooter(
+                      builder: (context, mode) {
+                        if (mode == LoadStatus.loading) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                        return const SizedBox();
+                      },
+                    ),
+                    controller: _refreshController,
+                    onLoading: () {
+                      context.read<ApiClientControllerCubit>().getGallery();
+                    },
+                    child: state is ApiClientControllerGalleryView
+                        ? GridView.builder(
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                                    mainAxisSpacing: 10,
+                                    crossAxisSpacing: 10,
+                                    mainAxisExtent: 200,
+                                    crossAxisCount: 2),
+                            itemCount: state.galleryViews.length,
+                            itemBuilder: (context, index) => GalleryViewCard(
+                                  data: state.galleryViews[index],
+                                  onTap: () {
+                                    _onGalleryViewClick(
+                                        context, state.galleryViews[index]);
+                                  },
+                                ))
+                        : const SizedBox());
+              },
+            ),
           ),
         ),
       ),
@@ -60,6 +95,6 @@ class ServiceView extends StatelessWidget {
 
   void _onGalleryViewClick(BuildContext context, GalleryView e) {
     Navigator.of(context).pushNamed(Routes.concreteViewer,
-        arguments: ConcreteViewerData(client: apiClient, uid: e.uid));
+        arguments: ConcreteViewerData(client: widget.apiClient, uid: e.uid));
   }
 }
