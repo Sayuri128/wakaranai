1c8f109015f7f085bc45921093d0fba5b975b0cb
armatura24 <armaturapengva2222@gmail.com>
2024-07-25 19:59:03 +0300

    upd translations

--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -570,6 +570,76 @@ class S {
     );
   }
 
+  /// `Clear activity history`
+  String get settings_clear_activity_history {
+    return Intl.message(
+      'Clear activity history',
+      name: 'settings_clear_activity_history',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Are you sure you want to clear your history?`
+  String get settings_clear_activity_history_dialog_confirmation_title {
+    return Intl.message(
+      'Are you sure you want to clear your history?',
+      name: 'settings_clear_activity_history_dialog_confirmation_title',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `This action cannot be canceled later`
+  String get settings_clear_activity_history_dialog_confirmation_message {
+    return Intl.message(
+      'This action cannot be canceled later',
+      name: 'settings_clear_activity_history_dialog_confirmation_message',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Delete`
+  String get settings_clear_activity_history_dialog_confirmation_ok_label {
+    return Intl.message(
+      'Delete',
+      name: 'settings_clear_activity_history_dialog_confirmation_ok_label',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Cancel`
+  String get settings_clear_activity_history_dialog_confirmation_cancel_label {
+    return Intl.message(
+      'Cancel',
+      name: 'settings_clear_activity_history_dialog_confirmation_cancel_label',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `History has been cleared`
+  String get settings_clear_activity_history_dialog_success {
+    return Intl.message(
+      'History has been cleared',
+      name: 'settings_clear_activity_history_dialog_success',
+      desc: '',
+      args: [],
+    );
+  }
+
+  /// `Error occurred during clearing history`
+  String get settings_clear_activity_history_dialog_error {
+    return Intl.message(
+      'Error occurred during clearing history',
+      name: 'settings_clear_activity_history_dialog_error',
+      desc: '',
+      args: [],
+    );
+  }
+
   /// `Done`
   String get web_browser_no_login_button {
     return Intl.message(
